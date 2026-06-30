b02592615375df1ed49e7e97542af7494cdac222
Ken Odegard <kodegard@anaconda.com>
2026-06-29 21:51:27 -0500

    Decouple CI from conda/conda

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2012 Anaconda, Inc
+# Copyright (C) 2023 conda
+# SPDX-License-Identifier: BSD-3-Clause
+"""Pytest configuration for conda-classic-solver."""
+
+pytest_plugins = (
+    "conda.testing",
+    "conda.testing.fixtures",
+)
